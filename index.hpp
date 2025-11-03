f8f662fef53b138b9b8834b67acdfc7110b6c963
jwild2027 <jwild2027@gmail.com>
2025-11-03 12:34:44 -0500

    Update index.hpp

--- index.hpp
+++ index.hpp
@@ -6,7 +6,6 @@
 #include <optional>
 #include "CityKey.hpp"
 
-namespace geo {
 
 struct PointLL {
     double lat{};
@@ -29,15 +28,12 @@ class SpatialIndex {
 public:
     void loadCSV(const std::string& path, bool logProgress = false);
 
-    std::optional<CenterLL> getCenter(const std::string& stateRaw,
-                                      const std::string& cityRaw) const;
+    std::optional<CenterLL> getCenter(const std::string& stateRaw, const std::string& cityRaw) const;
 
     std::vector<std::string> getStates() const;
     std::vector<std::string> getCities(const std::string& stateRaw) const;
 
-    std::vector<PointLL> queryKm(const std::string& stateRaw,
-                                 const std::string& cityRaw,
-                                 double radius_km) const;
+    std::vector<PointLL> queryKm(const std::string& stateRaw, const std::string& cityRaw,  double radius_km) const;
 
     std::size_t totalRowsLoaded() const { return loaded_; }
     std::size_t totalRowsSkipped() const { return skipped_; }
@@ -53,11 +49,8 @@ private:
     std::size_t loaded_{0};
     std::size_t skipped_{0};
 
-    static bool parseHeader(const std::string& header,
-                            int& idxCity, int& idxState, int& idxLat, int& idxLon);
-    static bool parseRowLite(const std::string& line,
-                             int idxCity, int idxState, int idxLat, int idxLon,
-                             RowLite& out);
+    static bool parseHeader(const std::string& header,  int& idxCity, int& idxState, int& idxLat, int& idxLon);
+    static bool parseRowLite(const std::string& line,  int idxCity, int idxState, int idxLat, int idxLon, RowLite& out);
 
     static double haversine_km(double lat1, double lon1, double lat2, double lon2);
     static inline double deg2rad(double x) { return x * 0.01745329251994329576923690768489; }
@@ -65,4 +58,4 @@ private:
     void computeCenters();
 };
 
-}
+
